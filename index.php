1081ac511d1a83e1bb093ebe6a238dfdad5374f1
OndrejCabyCabelka <79140324+OndrejCabyCabelka@users.noreply.github.com>
2021-03-16 11:05:34 +0100

    wp1-03

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+    $r = rand(1,100);
+    $π = "3,14";
+
+    // obvod
+    $sircuit = 2*$π*$r;
+
+    // obsah
+    $area = $π*$r*$r;
+
+    
+
+    ?>
+
+    <p>Obvod kruhu  = <?= $sircuit?> </p>
+    <p>Obsah kruhu  = <?= $area?> </p>
+    
+</body>
+</html>
